de4262f9e55f54ebafe36e8ada814e706e3d21d4
Cyph3redH <262104415+Cyph3redH@users.noreply.github.com>
2026-04-16 20:16:21 +0300

    Добавил кэш с Redis

---
+++ app/core/redis_client.py
@@ -0,0 +1,14 @@
+import os
+import redis.asyncio as redis
+
+REDIS_URL = os.getenv("REDIS_URL", "redis://redis:6379/0")
+
+redis_client = redis.from_url(REDIS_URL, decode_responses=True)
+
+async def is_cve_sent(cve_id: str) -> bool:
+    """Проверяет, была ли уже отправлена эта CVE."""
+    return await redis_client.exists(f"sent_cve:{cve_id}") > 0
+
+async def mark_cve_sent(cve_id: str, ttl: int = 604800):  # 7 дней
+    """Помечает CVE как отправленную на TTL секунд."""
+    await redis_client.setex(f"sent_cve:{cve_id}", ttl, "1")
--- app/tasks.py
+++ app/tasks.py
@@ -3,7 +3,7 @@ from app.core.celery_app import app
 from app.bot.alerts import send_alert
 from app.parser.nvd import search_critical_cve
 from app.parser.hackernews import fetch_dangerous_articles
-
+from app.core.redis_client import is_cve_sent, mark_cve_sent
 
 @app.task
 def check_all_sources():
@@ -15,16 +15,32 @@ async def _async_check():
     # Данные от NVD
     cves = search_critical_cve()
     for cve in cves:
+        cve_id = cve['cve_id']
+
+        if await is_cve_sent(cve_id):
+            print(f"⏭️ CVE {cve_id} уже отправлялась, пропускаем")
+            continue
+        
         msg = f"⚠️ Обнаружена критическая уязвимость в базе NVD</b>\n\n"
         msg += f"📛 <b>{cve['cve_id']}</b>\n"
         msg += f"❗ CVSS: {cve['cvss']}\n"
         msg += f"🔗 {cve['link']}"
         await send_alert(msg)
+        # Сохранение в кэш
+        await mark_cve_sent(cve_id)
     
     # Данные от Hacker News
     articles = fetch_dangerous_articles()
     for article in articles:
+
+        link = article['link']
+        
+        if await is_cve_sent(link):
+            continue
+
         msg = f"⚠️ <b>Опасная новость на Hacker News, просьба ознакомиться</b>\n\n"
         msg += f"📌 {article['title']}\n"
         msg += f"🔗 {article['link']}"
         await send_alert(msg)
+        # Сохранение в кэш
+        await mark_cve_sent(link)
