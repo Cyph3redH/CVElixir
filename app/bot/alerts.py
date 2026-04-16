8adebb188829891117b6f38a273d44948523186f
Cyph3redH <262104415+Cyph3redH@users.noreply.github.com>
2026-04-16 20:25:57 +0300

    Добавлена картинка к уведомлениям

--- app/bot/alerts.py
+++ app/bot/alerts.py
@@ -7,6 +7,7 @@ load_dotenv()
 
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 MY_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")  # личный ID
+HACKER_IMAGE_URL="https://images.interestingengineering.com/img/iea/QjOdpBaKOd/jbs-restoring-systems-with-backup-vulnerabilities-remain.jpg"
 
 # БЕЛЫЙ СПИСОК
 ALLOWED_USERS = [int(MY_CHAT_ID)]  # Можно добавить ещё ID через запятую
@@ -25,8 +26,12 @@ async def send_alert(message: str, target_chat_id: int = None):
     # Если цель не указана — шлём всем разрешённым
     if target_chat_id is None:
         for chat_id in ALLOWED_USERS:
-            await bot.send_message(chat_id=chat_id, text=message, parse_mode='HTML')
-            print(f"Отправлено для {chat_id}")
+            await bot.send_photo(
+                chat_id=chat_id,
+                photo=HACKER_IMAGE_URL,
+                caption=message,
+                parse_mode='HTML'
+            )
         return
     
     # Если цель указана — проверяем, есть ли она в белом списке
@@ -34,8 +39,12 @@ async def send_alert(message: str, target_chat_id: int = None):
         print(f"Доступ запрещён для {target_chat_id}")
         return
     
-    await bot.send_message(chat_id=target_chat_id, text=message, parse_mode='HTML')
-    print(f"Отправлено для {target_chat_id}")
+    await bot.send_photo(
+        chat_id=target_chat_id,
+        photo=HACKER_IMAGE_URL,
+        caption=message,
+        parse_mode='HTML'
+    )
 
 # Тест
 if __name__ == "__main__":
